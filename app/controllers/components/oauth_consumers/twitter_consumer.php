fbb31ccb268699de0949850a495df9896d18357e
nikitiki <nikitiki123456@yahoo.co.jp>
2010-05-15 22:49:21 +0900

    add twitter oauth

---
+++ app/controllers/components/oauth_consumers/twitter_consumer.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Twitter Consumer Component
+ *
+ * PHP 5
+ *
+ * @category Component
+ * @package  EP
+ * @version  1.0
+ * @author   Emmanuel P <hello@pozo.me>
+ * @license  http://www.opensource.org/licenses/mit-license.php The MIT License
+ * @link     http://github.com/Jewgonewild/Twitter-Oauth-CakePHP
+ */
+class TwitterConsumer extends AbstractConsumer {
+    public function __construct() {
+        parent::__construct('your_consumber_key','your_consumer_secret');
+    }
+}
+?>
